8ff922c02a051443fc4a81b31a7b9ff386a97c24
55555-Jyeon <jyeon380516@gmail.com>
2024-09-19 18:29:38 +0900

    ✨ initial: 디자인 토큰 및 초기 폴더 구조 세팅

---
+++ styles/color.ts
@@ -0,0 +1,21 @@
+export const COLORS = {
+  SYSTEM: {
+    black: "#0C0C0D",
+    white: "#ffffff",
+    red: "#FF1F00",
+  },
+  GREY: {
+    10: "#F8F9FC",
+    20: "#E3E5EB",
+    30: "#C3C6CC",
+    40: "#ABAFB7",
+    50: "#8D8F95",
+    60: "#727479",
+    70: "#5B5B5C",
+    80: "#434344",
+    90: "#282929",
+  },
+  BRAND: {
+    50: "#6317E5",
+  },
+} as const;
