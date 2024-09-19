1179caa3c3776655d5486e555e78cc0f6e0da61f
55555-Jyeon <jyeon380516@gmail.com>
2024-09-20 01:55:39 +0900

    🎁 styles: 레이아웃 퍼블리싱

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,4 +1,9 @@
 const Footer = () => {
-  return <div className="">footer</div>;
+  return (
+    <div className="absolute bottom-0 w-full h-[120px] border-t border-GREY-20 text-[12px] text-GREY-50 flex-center flex-col">
+      <p>Kim Dongkyun, Kim Jinsol, Moon Jiyoon, Oh Juyeon</p>
+      <p>all rights reserved &copy; 2024</p>
+    </div>
+  );
 };
 export default Footer;
