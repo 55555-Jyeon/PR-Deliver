a9b0fc2096714647a9aa6482973f2ce6a9a5913f
ijimnlosk <skek298@naver.com>
2024-09-26 15:59:16 +0900

    💡refactor: button type 추가

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -23,21 +23,25 @@ import "@/styles/button.css";
       />
  */
 const DeliverButton = ({
-  isActive = false,
-  onClick,
-  length,
-  label,
+    isActive = false,
+    onClick,
+    length,
+    label,
+    type = "button",
 }: DeliverButtonProps) => {
-  return (
-    <button
-      onClick={onClick}
-      className={`relative h-[52px] rounded-[10px] overflow-hidden group ${
-        isActive ? "text-GREY-10 bg-BRAND-50" : "text-GREY-20 bg-GREY-30"
-      } ${length === "full" ? "w-[416px]" : "w-[200px]"}`}
-    >
-      <span className="custom-hover-effect" />
-      <p className="relative z-10">{label}</p>
-    </button>
-  );
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={`relative h-[52px] rounded-[10px] overflow-hidden group ${
+                isActive
+                    ? "text-GREY-10 bg-BRAND-50"
+                    : "text-GREY-20 bg-GREY-30"
+            } ${length === "full" ? "w-[416px]" : "w-[200px]"}`}
+        >
+            <span className="custom-hover-effect" />
+            <p className="relative z-10">{label}</p>
+        </button>
+    );
 };
 export default DeliverButton;
--- components/common/type.ts
+++ components/common/type.ts
@@ -1,22 +1,23 @@
 import { UseControllerProps } from "react-hook-form";
 
 export type StepTemplateProps = {
-  index: number;
-  stepDesc: string;
-  imageSrc: string;
-  imageHeight: number;
-  imageDesc?: string;
+    index: number;
+    stepDesc: string;
+    imageSrc: string;
+    imageHeight: number;
+    imageDesc?: string;
 };
 
 export type DeliverButtonProps = {
-  isActive?: boolean;
-  onClick: () => void;
-  length: "smaller" | "full";
-  label: string;
+    isActive?: boolean;
+    onClick?: () => void;
+    length: "smaller" | "full";
+    label: string;
+    type?: "button" | "submit";
 };
 
 export type CustomInputProps = {
-  title?: string;
-  placeholder: string;
-  className?: string;
+    title?: string;
+    placeholder: string;
+    className?: string;
 } & UseControllerProps;
