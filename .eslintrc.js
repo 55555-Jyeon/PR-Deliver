c033cc8a973ef487577c5bae9d8f94ba2c72972b
55555-Jyeon <jyeon380516@gmail.com>
2024-11-02 22:58:35 +0900

    ✨ initial: es-lint 설정 추가(import 순서)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,54 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "react"],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "no-console": "warn",
-    "no-unused-vars": "warn",
-    "prettier/prettier": "error",
-  },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "react"],
+    rules: {
+        indent: ["error", "tab"],
+        "linebreak-style": ["error", "windows"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-console": "warn",
+        "no-unused-vars": "warn",
+        "prettier/prettier": "error",
+        "import/order": [
+            "error",
+            {
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    ["sibling", "parent"],
+                    "index",
+                ],
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
+            },
+        ],
+    },
 };
