c26d020095bccbbdc51525e990610727bab633fe
ijimnlosk <skek298@naver.com>
2024-12-09 22:46:59 +0900

    🐞fix: layout 위치 조정

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -3,12 +3,12 @@ import Header from "./header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col">
             <Header />
             <main className="h-full flex-1 overflow-y-scroll hide-scrollbar">
                 <div className="h-full mt-[60px]">{children}</div>
-                <Footer />
             </main>
+            <Footer />
         </div>
     );
 };
